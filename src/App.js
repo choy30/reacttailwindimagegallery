fcc5b017ada31b38c474404cb8ef86e0b3892adb
devYeonjun <alcls0026@gmail.com>
2022-05-31 18:37:35 +1200

    Add search function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import ImageCard from "./componenets/ImageCard";
+import ImageSearch from "./componenets/ImageSearch";
 
 function App() {
 	const [images, setImages] = useState([]);
@@ -17,10 +18,11 @@ function App() {
 				setIsLoading(false);
 			})
 			.catch((err) => console.log(err));
-	}, []);
+	}, [term]);
 
 	return (
 		<div className="container mx-auto">
+			<ImageSearch searchText={(text) => setTerm(text)} />
 			{isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
 				{images.map((image) => (
 					<ImageCard key={image.id} image={image}/>
