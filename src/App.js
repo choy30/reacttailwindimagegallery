bbcf87d7f2b458e167d2bf662e25113b8076ba2c
devYeonjun <alcls0026@gmail.com>
2022-05-31 18:24:25 +1200

    Add imagecard componenet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 
+import ImageCard from "./componenets/ImageCard";
+
 function App() {
-	const [iamges, setImages] = useState([]);
+	const [images, setImages] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 	const [term, setTerm] = useState("");
 
@@ -11,43 +13,18 @@ function App() {
 		)
 			.then((res) => res.json())
 			.then((data) => {
-				setImages(data.hiys);
+				setImages(data.hits);
 				setIsLoading(false);
 			})
 			.catch((err) => console.log(err));
 	}, []);
+
 	return (
-		<div className="mx-w-sm rounded overflow-hidden shadow-lg">
-			<img src="https://source.unsplash.com/random" alt="" className="w-full" />
-			<div className="px-6 py-4">
-				<div className="font-bold text-purple-500 text-xl mb-2">
-					Photo by Ranbdom
-				</div>
-				<ul>
-					<li>
-						<strong>View:</strong>
-						4000
-					</li>
-					<li>
-						<strong>Downloads:</strong>
-						300
-					</li>
-					<li>
-						<strong>Likes:</strong>
-						400
-					</li>
-				</ul>
-			</div>
-			<div className="px-6 py-4">
-				<span className="inline-block bg-gray-200 rounded-full px-3 py-1 test-sm font-semibold text-gray-700 mr-2">
-					#tag1
-				</span>
-				<span className="inline-block bg-gray-200 rounded-full px-3 py-1 test-sm font-semibold text-gray-700 mr-2">
-					#tag2
-				</span>
-				<span className="inline-block bg-gray-200 rounded-full px-3 py-1 test-sm font-semibold text-gray-700 mr-2">
-					#tag3
-				</span>
+		<div className="container mx-auto">
+			<div className="grid grid-cols-3 gap-4">
+				{images.map((image) => (
+					<ImageCard key={image.id} image={image}/>
+				))}
 			</div>
 		</div>
 	);
